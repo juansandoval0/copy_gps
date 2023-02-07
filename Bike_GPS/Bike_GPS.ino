543c74557213aa2f913dbd726f2e03e8a749ac51
bultodepapas <bultodepapas@gmail.com>
2023-02-07 18:45:16 -0500

    Update Bike_GPS.ino

--- Bike_GPS/Bike_GPS.ino
+++ Bike_GPS/Bike_GPS.ino
@@ -39,16 +39,9 @@ SoftwareSerial ss(RXPin, TXPin);
 void setup() {
 
   //inciar leds
-  delay(3000);  // 3 second delay for recovery
-
-  // tell FastLED about the LED strip configuration
+  delay(3000); 
   FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-  //FastLED.addLeds<LED_TYPE,DATA_PIN,CLK_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-
-  // set master brightness control
   FastLED.setBrightness(BRIGHTNESS);
-
-
 
 
 
